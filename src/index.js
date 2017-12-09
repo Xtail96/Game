46b65dc8a48ec22b5fca9b9c9a6b42a26f883d00
Xtail <xtail1996@yandex.ru>
2017-12-09 19:10:42 +0300

    Прототип меню

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function initGameField() {
     let context = canvas.getContext('2d');
     canvas.width = 1366;
     canvas.height = 700;
-    canvas.style.border = '1px solid #333';
+    //canvas.style.border = '1px solid transparent';
     //canvas.style.backgroundColor = '#eee';
     canvas.style.display = 'block';
 
@@ -39,31 +39,39 @@ function initGameField() {
     }
     mapManager.draw(context);*/
 
-    let gameManager = new GameManager(canvas, context, canvas.width, canvas.height, 10, 100, 5, 1/10, 1, 5, 7, 3);
+    initGameMenu(canvas.width);
+
+    let gameManager = new GameManager(canvas, context, canvas.width, canvas.height, 10, 100, 5, 1/10, 1/25, 5, 7, 3);
     gameManager.loadAll();
     gameManager.play();
 
 }
 
-function createGameMenu() {
+function createGameMenu(canvasWidth) {
     let menu = document.createElement('div');
     menu.id = 'mainMenu';
-    menu.style.backgroundColor = '#333';
+    menu.style.backgroundColor = '#fff';
     menu.style.display = 'block';
-    menu.style.width = 1366;
-    menu.style.height = 50;
-    menu.style.border = '1px solid #333';
+    menu.style.width = canvasWidth;
+    menu.style.height = 25;
+    menu.style.padding = 10;
+    //menu.style.border = '1px solid #eee';
 
-    let buttons = createMenuButtons();
+    /*let buttons = createMenuButtons();
     for(let i in buttons){
         menu.appendChild(buttons[i]);
+    }*/
+
+    let dashboardElements = createDashboard();
+    for(let i in dashboardElements) {
+        menu.appendChild(dashboardElements[i]);
     }
 
     return menu;
 }
 
-function initGameMenu() {
-    document.body.appendChild(createGameMenu());
+function initGameMenu(canvasWidth) {
+    document.body.appendChild(createGameMenu(canvasWidth));
 }
 
 function createMenuButtons() {
@@ -86,5 +94,59 @@ function createMenuButtons() {
     return buttons;
 }
 
-initGameMenu();
+function createDashboard() {
+    let elements = [];
+
+    let playerSizeLabel = document.createElement('b');
+    playerSizeLabel.id = 'playerSizeLabel';
+    playerSizeLabel.textContent = 'Размер игрока: ';
+    elements.push(playerSizeLabel);
+
+    let playerSize = document.createElement('input');
+    playerSize.id = 'playerSizeLabel';
+    playerSize.setAttribute('type', 'text');
+    playerSize.setAttribute('readonly', '');
+    playerSize.setAttribute('value', '0');
+    elements.push(playerSize);
+
+
+    let targetSizeLabel = document.createElement('b');
+    targetSizeLabel.id = 'targetSizeLabel';
+    targetSizeLabel.textContent = 'Цель: ';
+    elements.push(targetSizeLabel);
+
+    let targetSize = document.createElement('input');
+    targetSize.id = 'targetPlayerSize';
+    targetSize.setAttribute('type', 'text');
+    targetSize.setAttribute('readonly', '');
+    targetSize.setAttribute('value', '0');
+    elements.push(targetSize);
+
+    let plantCountLabel = document.createElement('b');
+    plantCountLabel.id = 'plantCountLabel';
+    plantCountLabel.textContent = 'Количество растений: ';
+    elements.push(plantCountLabel);
+
+    let plantsCount = document.createElement('input');
+    plantsCount.id = 'plantCount';
+    plantsCount.setAttribute('type', 'text');
+    plantsCount.setAttribute('readonly', 'true');
+    plantsCount.setAttribute('value', '0');
+    elements.push(plantsCount);
+
+    let enemyCountLabel = document.createElement('b');
+    enemyCountLabel.id = 'enemyCountLabel';
+    enemyCountLabel.textContent = 'Количество соперников: ';
+    elements.push(enemyCountLabel);
+
+    let enemyCount = document.createElement('input');
+    enemyCount.id = 'enemyCount';
+    enemyCount.setAttribute('type', 'text');
+    enemyCount.setAttribute('readonly', 'true');
+    enemyCount.setAttribute('value', '0');
+    elements.push(enemyCount);
+
+    return elements;
+}
+
 initGameField();
